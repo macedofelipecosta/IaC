93dc31334efa54175f5ffc96ab96719a7f8ebf23
Felipe <macedofelipecosta@icloud.com>
2025-06-25 19:03:59 -0300

    En el output del modulo network se agrega .arn a los tg de result y vote

--- modules/load_balancer/outputs.tf
+++ modules/load_balancer/outputs.tf
@@ -9,10 +9,10 @@ output "alb_result_dns" {
 
 output "target_group_arn_vote" {
   description = "devuelve el arn del tg vote"
-  value= aws_alb_target_group.alb_vote_tg
+  value= aws_alb_target_group.alb_vote_tg.arn
 }
 
 output "target_group_arn_result" {
   description = "devuelve el arn del tg result"
-  value= aws_alb_target_group.alb_result_tg
+  value= aws_alb_target_group.alb_result_tg.arn
 }
