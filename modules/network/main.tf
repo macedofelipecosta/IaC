e097addc6c690fe601d7c9cb4f8f1730cb5c61db
Felipe <macedofelipecosta@icloud.com>
2025-06-25 13:42:22 -0300

    Se crea modulo network con recursos vpc, subnets publicas y privadas, igw, nat gtw, y tablas de ruteo publicas y privadas

---
+++ modules/network/main.tf
@@ -0,0 +1,113 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
+
+resource "aws_subnet" "public_subnet" {
+  count = length(var.public_subnets)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    environment = var.environment
+    Name = "${var.vpc_name}-public-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  count = length(var.private_subnets)
+
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.vpc_name}-private-${count.index + 1}"
+    environment= var.environment
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  count = length(var.public_subnets) #Crea una NAT GTW por cada subred publica
+
+  allocation_id = aws_eip.nat_eip[count.index].id #Se asocia una EIP a cada NAT Gateway
+  subnet_id     = aws_subnet.public_subnet[count.index].id
+
+  tags = {
+    Name = "${var.vpc_name}-nat-gw-${count.index + 1}"
+    environment = var.environment
+  }
+}
+
+#el nat eip asigna ip publica a nat gtw, le permite la salida a internet.
+resource "aws_eip" "nat_eip" {
+  count = length(var.public_subnets) #Crea una EIP por cada subred publica
+  domain = "vpc"
+  tags = {
+    Name = "${var.vpc_name}-nat-eip-${count.index + 1}"
+    environment = var.environment
+  }
+}
+
+#PUBLIC ROUTE
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  
+  tags = {
+    Name = "${var.vpc_name}-public-route-table"
+    environment = var.environment
+  }
+}
+resource "aws_route" "public_route" {
+  route_table_id         = aws_route_table.public_route_table.id
+  destination_cidr_block = "0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+resource "aws_route_table_association" "public_subnet_association" {
+  count = length(var.public_subnets)
+  subnet_id      = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+#PRIVATE ROUTE 
+
+resource "aws_route_table" "private_route_table" {
+  
+  #count = length(var.private_subnets)
+  vpc_id = aws_vpc.main.id
+  
+  tags = {
+    #Name = "${var.vpc_name}-private-route-table-${count.index + 1}"
+    Name = "${var.vpc_name}-private-route-table"
+    environment = var.environment
+  }
+}
+resource "aws_route" "private_route" {
+  #count = length(var.private_subnets)
+
+  #route_table_id         = aws_route_table.private_route_table[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+  destination_cidr_block = "0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_gw[count.index].id
+}
+
+resource "aws_route_table_association" "private_subnet_association" {
+  #count = length(var.private_subnets)
+  subnet_id      = aws_subnet.private_subnet[count.index].id
+  #route_table_id = aws_route_table.private_route_table[count.index].id
+  route_table_id= aws_route_table.private_route_table.id
+}
---
+++ modules/network/outputs.tf
@@ -0,0 +1,32 @@
+output "vpc_id"{
+  value=aws_vpc.main.id
+}
+output "vpc_cidr_block"{
+  value=aws_vpc.main.cidr_block
+}
+output "public_subnet_ids"{
+  value=aws_subnet.public_subnet[*].id
+}
+
+output "private_subnet_ids"{
+  value=aws_subnet.private_subnet[*].id
+}
+
+output "aws_natgateway_id"{
+  value=aws_nat_gateway.nat_gateway.id
+}
+
+output "aws_internetgateway_id"{
+  value=aws_internet_gateway.internet_gateway.id
+}
+
+output "aws_eip_id"{
+  value=aws_eip.eip.id
+}
+
+output "aws_route_table_public_id"{
+  value=aws_route_table.public_route_table.id
+}
+output "aws_route_table_private_id"{
+  value=aws_route_table.private_route_table.id
+}
