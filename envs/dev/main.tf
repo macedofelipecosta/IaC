8cb41b7aee8b43174ccc66c02dc5d2348ece0eaa
Felipe <macedofelipecosta@icloud.com>
2025-06-25 17:40:28 -0300

    Se comenta el ignore de *.tfvars

---
+++ envs/dev/main.tf
@@ -0,0 +1,81 @@
+data "aws_iam_role" "labrole" {
+  name = "LabRole"
+}
+
+module "cluster" {
+  source             = "../../modules/ecs_cluster"
+  cluster_name       = var.cluster_name
+  environment        = var.environment
+  capacity_providers = var.capacity_providers
+}
+
+module "network" {
+  source             = "../../modules/network"
+  vpc_name           = var.vpc_name
+  vpc_cidr_block     = var.vpc_cidr_block
+  public_subnets     = var.public_subnet_cidr_blocks
+  private_subnets    = var.private_subnet_cidr_blocks
+  availability_zones = var.azs
+  environment        = var.environment
+
+}
+
+module "security" {
+  source      = "../../modules/security"
+  environment = var.environment
+  vpc_id      = module.network.vpc_id
+
+}
+
+module "load_balancer" {
+  source             = "../../modules/load_balancer"
+  vpc_id             = module.network.vpc_id
+  private_subnets_id = module.network.private_subnet_ids
+  app_sg_id          = module.security.app_sg
+}
+
+module "ecs_vote" {
+  source                = "../../modules/ecs_vote"
+  environment           = var.environment
+  private_subnet_ids    = module.network.private_subnet_ids
+  cluster_id            = module.cluster.cluster_id
+  app_sg                = module.security.app_sg
+  target_group_arn_vote = module.load_balancer.target_group_arn_vote
+  vote_image            = var.vote_image
+  role_arn              = data.aws_iam_role.labrole.arn
+}
+
+module "ecs_result" {
+  source                  = "../../modules/ecs_result"
+  environment             = var.environment
+  private_subnets_id      = module.network.private_subnet_ids
+  app_sg                  = module.security.app_sg
+  cluster_id              = module.cluster.cluster_id
+  target_group_arn_result = module.load_balancer.target_group_arn_result
+  result_image            = var.result_image
+  role_arn                = data.aws_iam_role.labrole.arn
+}
+
+module "ecs_worker" {
+  source             = "../../modules/ecs_worker"
+  environment        = var.environment
+  private_subnet_ids = module.network.private_subnet_ids
+  app_sg             = module.security.app_sg
+  cluster_id         = module.cluster.cluster_id
+  worker_image       = var.worker_image
+  role_arn           = data.aws_iam_role.labrole.arn
+}
+
+module "redis" {
+  source             = "../../modules/redis"
+  environment        = var.environment
+  private_subnet_ids = module.network.private_subnet_ids
+  databases_sg       = module.security.databases_sg
+}
+
+module "postgres" {
+  source             = "../../modules/postgres"
+  environment        = var.environment
+  databases_sg       = module.network.databases_sg
+  private_subnet_ids = module.network.private_subnet_ids
+}
