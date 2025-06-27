725b3158455d2488bab44c4757a43e796dc0a012
Felipe <macedofelipecosta@icloud.com>
2025-06-27 16:51:10 -0300

    Se formatean los archivos terraform

--- envs/main/bucket.tf
+++ envs/main/bucket.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "states-terraform-217821"
-    key    = "main/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "states-terraform-217821"
+    key     = "main/terraform.tfstate"
+    region  = "us-east-1"
     encrypt = true
   }
 }
--- envs/test/bucket.tf
+++ envs/test/bucket.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "states-terraform-217821"
-    key    = "test/terraform.tfstate"
-    region = "us-east-1"
+    bucket  = "states-terraform-217821"
+    key     = "test/terraform.tfstate"
+    region  = "us-east-1"
     encrypt = true
   }
 }
